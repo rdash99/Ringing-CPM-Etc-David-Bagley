0fb2b4cd7b7d41446aeff3a202ac40583368cdc3
Rowan Dash <pokemonmaster9967@gmail.com>
2022-02-21 14:25:35 +0000

    Update methodProcessing.py

--- methodProcessing.py
+++ methodProcessing.py
@@ -1,30 +1,32 @@
 # todo - reimplement in Java to test speed of requests
+
+import os
 import time
-import requests
-import json
-
 import sqlite3
-db = sqlite3.connect('Methods.db')
-cur = db.cursor()
+import requests
+import multiprocessing
+import json
+import shutil
 
 
-def createTable():
-    try:
-        cur.execute(
-            "CREATE TABLE Methods (id integer PRIMARY KEY, library text, title text, name text, stage integer, placeNotation text, rows blob)")
-        db.commit()
-        print('Table created')
-    except:
-        pass
+def createTable(cur, db):
+    cur.execute(
+        "CREATE TABLE Methods (id integer PRIMARY KEY, library text, title text, name text, stage integer, placeNotation text, rows text)")
+    db.commit()
+    #print('Table created')
 
 
-def dropTable():
-    try:
-        cur.execute("DROP TABLE if exists Methods")
-        db.commit()
-        print('Table dropped')
-    except:
-        pass
+def dropTable(cur, db):
+    cur.execute("DROP TABLE if exists Methods")
+    db.commit()
+    #print('Table dropped')
+
+
+def chunk(lst, amt):
+    lstChunk = []
+    for i in range(0, len(lst), amt):
+        lstChunk.append(lst[i: i + amt])
+    return lstChunk
 
 
 def getAllMethods():
@@ -36,8 +38,31 @@ def getAllMethods():
     print(count)
     print(len(data['methods']))
 
+    chunks = chunk(data['methods'], 500)
+    print(len(chunks))
+    # print(chunks[0])
+    # print(processChunk(chunks[0]))
+
+    processes = []
+
+    chunkLength = len(chunks)
+    i = 0
+
+    for item in chunks:
+        p = multiprocessing.Process(target=processChunk, args=(item, i,))
+        p.name = str(i)
+        p.start()
+        processes.append(p)
+        i += 1
+
+    for process in processes:
+        process.join()
+    """ pool = multiprocessing.Pool(len(chunks))
+    pool.map(processChunk, chunks)
+    pool.close() """
+
     # todo - speed up this process by using multiprocessing and split the data into chunks before processing then write each chunk to the database
-    for method in data['methods']:
+    """ for method in data['methods']:
         id = method['id']
         library = method['library']
         title = method['title']
@@ -47,7 +72,11 @@ def getAllMethods():
         rows = getMethodRows(id)
         entity = (id, library, title, name, stage, placeNotation, rows)
 
-        methods.append(entity)
+        methods.append(entity) """
+
+    print("First stage complete")
+
+    writeToDatabase(chunkLength)
     print("done")
 
 
@@ -59,7 +88,85 @@ def getMethodRows(id):
     return rows
 
 
-start_time = time.time()
+def processChunk(chunk, name):
+    db = sqlite3.connect('./data/{}.db'.format(name))
+    cur = db.cursor()
+
+    dropTable(cur, db)
+    createTable(cur, db)
+
+    items = []
+    for item in chunk:
+        id = item['id']
+        library = item['library']
+        title = item['title']
+        name = item['name']
+        stage = item['stage']
+        placeNotation = item['placeNotation']
+        rows = getMethodRows(id)
+        entity = (id, library, title, name, stage, placeNotation, str(rows))
+
+        cur.execute(
+            "INSERT INTO Methods(id, library, title, name, stage, placeNotation, rows) VALUES(?, ?, ?, ?, ?, ?, ?)", entity)
+        db.commit()
+
+        items.append(entity)
+    db.close()
+    # return items
+
+
+def writeToDatabase(numItems):
+    db = sqlite3.connect('Methods.db')
+    cur = db.cursor()
+    dropTable(cur, db)
+    createTable(cur, db)
+    for i in range(numItems):
+        cur.execute("ATTACH DATABASE './data/{}.db' AS db{}".format(i, i))
+        db.commit()
+        cur.execute("INSERT INTO Methods SELECT * FROM db{}.Methods".format(i))
+        db.commit()
+        cur.execute("DETACH DATABASE db{}".format(i))
+        db.commit()
+
+    db.close()
+    cleanup()
+
+
+def main():
+    # dropTable()
+    # createTable()
+    start_time = time.time()
+    getAllMethods()
+    print("--- %s seconds ---" % (time.time() - start_time))
+    # print(processChunk(data['methods']))
+    # print(getMethodRows(1))
+
+
+""" start_time = time.time()
 getAllMethods()
-print("--- %s seconds ---" % (time.time() - start_time))
+print("--- %s seconds ---" % (time.time() - start_time)) """
+
+
+def createDirectory(path):
+    try:
+        os.makedirs(path)
+    except:
+        pass
+
+
+def removeDirectory(path):
+    try:
+        shutil.rmtree(path)
+    except:
+        pass
+
+
+def cleanup():
+    removeDirectory('./data')
+    createDirectory('./data')
+
+
+if __name__ == '__main__':
+    createDirectory('./data')
+    main()
 # getMethodRows(11795)
