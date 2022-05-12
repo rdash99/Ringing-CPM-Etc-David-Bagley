c53ae13aab6dd4ec648b757861ddc922523d5c32
Rowan Dash <pokemonmaster9967@gmail.com>
2022-05-12 20:21:14 +0100

    Update node.py

--- node.py
+++ node.py
@@ -1,12 +1,19 @@
 from p2pnetwork.node import Node
 from p2pnetwork.nodeconnection import NodeConnection
+import serial
 
 
 class BellNode (Node):
+    last_message = None
+    serialPort = None
+    serialConnection = None
+    callibrationMode = False
     # Python class constructor
-    def __init__(self, host, port, id, callback=None, max_connections=0):
+
+    def __init__(self, host, port, id, callibrationMode=False, callback=None, max_connections=0):
         super(BellNode, self).__init__(
             host, port, id, callback, max_connections)
+        self.callibrationMode = callibrationMode
 
     def outbound_node_connected(self, connected_node):
         print("outbound_node_connected: " + connected_node.id)
@@ -22,16 +29,13 @@ class BellNode (Node):
 
     def node_message(self, connected_node, data):
         print("node_message from " + connected_node.id + ": " + str(data))
+        last_message = data
 
     def node_disconnect_with_outbound_node(self, connected_node):
-        print("node wants to disconnect with oher outbound node: " + connected_node.id)
+        print("node wants to disconnect with other outbound node: " + connected_node.id)
 
     def node_request_to_stop(self):
         print("node is requested to stop!")
 
-    # OPTIONAL
-    # If you need to override the NodeConection as well, you need to
-    # override this method! In this method, you can initiate
-    # you own NodeConnection class.
     def create_new_connection(self, connection, id, host, port):
         return NodeConnection(self, connection, id, host, port)
